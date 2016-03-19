93e05afbe4df6db45cea9476393636ef1dc1962b
Nathan Douglas <nd9600@gmail.com>
2016-03-19 23:40:29 +0000

    Fixed the API call to work with coms with commas

--- yahoo_api.js
+++ yahoo_api.js
@@ -26,10 +26,13 @@ request.get(urlString, function (error, response, body) {
         var csvDictionary = {};
 
         for (var i = 0; i < csvArray.length; i++) {
-            var triple = csvArray[i].split(",");
-            var name = triple[0].substring(1, (triple[0].length - 1));
-            var ask = triple[1];
-            var bid = triple[2];
+            var splitList = csvArray[i].split(",");
+
+            var name = splitList.slice(0, (splitList.length - 2)).join();
+            name = name.substring(1, (name.length - 1));
+
+            var ask = splitList[(splitList.length - 2)];
+            var bid = splitList[(splitList.length - 1)];
 
             csvDictionary[name] = [bid, ask];
         }
