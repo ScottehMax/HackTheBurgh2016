4ab7914537301cd5da168cf2d0b5f8bd9aa716cd
Scott Maxwell <scott@scotteh.me>
2016-03-20 11:34:02 +0000

    hp bar colours
    yaaaaaaaaaaaaaaaaAAAAAAAAASSSSSSSSSSSS

--- global.js
+++ global.js
@@ -6,9 +6,9 @@ var stocks = { 'Alphabet Inc.': [ '736.80', '737.60', 'images/google_pixel.png',
   'Amazon.com, Inc.': [ '551.70', '552.75', 'images/pixel_amazon_logo.png',"images/circle-256.gif" ],
   'Microsoft Corporation': [ '53.41', '53.64', 'images/pixel_microsoft_logo.png',"images/circle-256.gif" ],
   'Facebook, Inc.': [ '111.35', '111.47', 'images/pixel_facebook_logo.png',"images/circle-256.gif" ],
-  'BlackRock, Inc. Common Stock': [ '342.52', '342.79', 'images/blackrock_logo.png',"images/circle-256.gif" ],
+  'BlackRock, Inc.': [ '342.52', '342.79', 'images/blackrock_logo.png',"images/circle-256.gif" ],
   'ARM Holdings plc': [ '39.00', '45.00', 'images/arm_logo.png',"images/circle-256.gif" ],
-  'JP Morgan Chase & Co. Common St': [ '60.40', '60.48', 'images/jpmorgan_logo.png',"images/circle-256.gif" ] }
+  'JP Morgan Chase & Co.': [ '60.40', '60.48', 'images/jpmorgan_logo.png',"images/circle-256.gif" ] }
 
 var i, Global = {
   battles: {},
